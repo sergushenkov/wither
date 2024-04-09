eb8696c261427165c5cf7dcbdabec77aea5c3043
sergushenkov <sergushenkov@gmail.com>
2024-04-09 17:16:16 +0300

    step 09

--- step_08.py
+++ step_08.py
@@ -2,14 +2,22 @@ class Monster:
     def __init__(self, name, health, damage, speed):
         self.name = name
         self.health = health
-        self.damage = damage
-        self.speed = speed
-        self.state = (
-            "roaming"  # attacking, fleeing, sleeping, trapped, controlled, roaming
-        )
+        # урон и скорость для монстров разного типа хранятся в отдельной таблице
+        # self.damage = damage  
+        # self.speed = speed
 
     def next_action(self):
-        """определяет следующее действие монстра"""
+        """определяет следующее действие монстра - куда и на какое расстояние переместится 
+        до - монстр жив
+        """
+
+    def hurt(self, damage):
+        """монстр получает урон
+        до - монстр жив
+        после - если здоровье меньше равно нулю - монстр исчезает"""
+        self.health -= damage
+        if self.health <= 0:
+            self.die()
 
 
 class Items:
@@ -23,12 +31,11 @@ class Items:
 
 
 class Weapon(Items):
-    def __init__(self, name, weight, damage, range, damage_type):
+    def __init__(self, name, weight, damage, range):
         self.name = name
         self.weight = weight
         self.damage = damage
         self.range = range
-        self.damage_type = damage_type  # cutting, piercing, blunt
 
 
 class Armor(Items):
@@ -50,38 +57,66 @@ class Player:
     def __init__(self, armor, weapon):
         self.health = 100
         self.mana = 100
-        self.current_armor = armor
-        self.current_weapon = weapon
-        self.bag = Bag()  # TODO: bag should be a list of items
+        self.bag = Bag()
 
     def next_action(self):
         """запрашивает действие пользователя и возвращает его"""
         pass
 
-    def pick_up(self, item):
-        """добавляет предмет в мешок"""
-        pass
-
     def use_item(self, item):
-        """использует предмет"""
+        """использует предмет
+        до - предмет есть в мешке
+        после - предмет оказал воздействие, исчез из мешка"""
         pass
 
     def attack(self, weapon, monster):
-        """атакует монстра"""
+        """атакует монстра
+        до - рядом есть монстр
+        после - монстр получил урон"""
         pass
 
-    def change_armor(self, armor):
-        """смена доспеха"""
-        pass
+    def hurt(self, damage):
+        """игрок получает урон
+        до - игрок жив
+        после - если здоровье меньше равно нулю - игра закончена исчезает"""
+        self.health -= damage
+        if self.health <= 0:
+            self.die()
 
-    def change_weapon(self, weapon):
-        """смена оружия"""
-        pass
-
-    def conjuire(self, sign):
+    def conjuire(self, sign, target):
         """активировать знак"""
+        pass
+
+    def drop(self, item):
+        """бросить предмет
+        до - предмет есть в мешке
+        после - предмет исчез из мешка, оказался на карте в текущей позиции или рядом"""
         pass
 
 class Bag():
     def __init__(self):
         self.items = []
+        self.capacity = 100
+        self.free_volume = 100
+        self.current_armor = armor
+        self.current_weapon = weapon
+        
+    def pick_up(self, item):
+        """добавляет предмет в мешок"""
+        pass
+
+    def extract(self, item):
+        """извлекает предмет из мешка"""
+        pass
+
+    def change_armor(self, armor):
+        """смена доспеха
+        до - в инвентаре есть доспех
+        после - доспех стал current_armor, исчез из инвентаря, добавился в инвентарь старый доспех"""
+        pass
+
+    def change_weapon(self, weapon):
+        """смена оружия
+        до - в инвентаре есть оружие
+        после - оружие стал current_weapon, исчез из инвентаря, добавился в инвентарь старое оружие"""
+        pass
